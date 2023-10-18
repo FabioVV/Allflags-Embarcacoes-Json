0c5db57262edeba51381aaa1dd2d95bf507bd0f2
FabioVV <fabio-varela@protonmail.com>
2023-10-18 13:10:23 -0300

    Fixed some parts of the code

--- GeradorJsonEmbarcacoes.py
+++ GeradorJsonEmbarcacoes.py
@@ -5,20 +5,24 @@ import os
 ## Conexão e SELECT com o banco
 
 mydatabase = MC.connect(
-    host='localhost',
+    host='srv-dev',
     user='root',
-    password='root',
+    password='',
     database='allflags'
 )
 
 
 sql = mydatabase.cursor()
+print('Conectado ao banco.')
 
 
 sql.execute("""
-        SELECT id, Nome, marca FROM embarc 
+        SELECT id_embarcacao, modelo, motor FROM embarcacoes 
 """)
 results_sql = sql.fetchall()
+
+#  print(results_sql)
+# exit()
 
 
 ## Váriaveis
@@ -26,7 +30,7 @@ results_sql = sql.fetchall()
 #Caso precise
 main_path = ''
 
-embarcacoes = {'embarcacoes':[]} 
+embarcacoes = []
 cwd = os.getcwd()  
 images_path = f"{cwd}/images"
 videos_path = f"{cwd}/videos"
@@ -37,37 +41,37 @@ dir_list_videos = os.listdir(videos_path)
 
 ######################################################## OPERAÇÕES ABAIXO
 
-for embarc in results_sql:
-    embarcacoes['embarcacoes'].append({
-        'id':embarc[0], 
-        'nome':embarc[1], 
-        'marca':embarc[2],
+for embarcacao in results_sql:
+    embarcacoes.append({
+        'id_embarcacao':embarcacao[0], 
+        'modelo':embarcacao[1], 
+        'motor':embarcacao[2],
         'imagens':[],
         'videos':[]
     })
 
-
+# print(embarcacoes)
 
 
 
 # Adiciona caminho das imagens e vídeos ao JSON das embarcações
 
-for embarc in embarcacoes['embarcacoes']:
+for embarc in embarcacoes:
     for directory in dir_list_images:
-        if embarc['marca'] == directory.upper():
+        if embarc['modelo'].upper() == directory.upper():
             images = os.listdir(f'{images_path}/{directory.upper()}')
             for image in images:
                 embarc['imagens'].append({'path':f'{images_path}/{directory.upper()}/{image}'})
 
     for directory in dir_list_videos:
-        if embarc['marca'] == directory.upper():
+        if embarc['modelo'].upper() == directory.upper():
             videos = os.listdir(f'{videos_path}/{directory.upper()}')
             for video in videos:
                 embarc['videos'].append({'path':f'{videos_path}/{directory.upper()}/{video}'})
 
 
 
-# Cria arquivo JSON baseado no dicionário de embarcações
+## Cria arquivo JSON baseado no dicionário de embarcações
 
 with open("embarcacoes.json", "w") as arquivo:
     json.dump(embarcacoes, arquivo, indent=4)
