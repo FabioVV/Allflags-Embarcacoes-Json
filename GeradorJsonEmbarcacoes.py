8c52a7d9e5b83d614f1e160e07d9ef9b21603dd0
FabioVV <fabio-varela@protonmail.com>
2023-11-22 13:56:37 -0300

    WIP fix

--- GeradorJsonEmbarcacoes.py
+++ GeradorJsonEmbarcacoes.py
@@ -3,193 +3,155 @@ import json
 import os
 
 
- # Main variables
+ ##  Main variables
 work_path = os.getcwd()  
 images_path = f"{work_path}/images"
 videos_path = f"{work_path}/videos"
 dir_list_images = os.listdir(images_path)  
 dir_list_videos = os.listdir(videos_path)  
 
+ # Variables to create the JSON files
 embarcacoes_novo_seminovo = []
 embarcacoes_share_modelo = []
 embarcacoes_share_localicazao = []
 
 
-
-# Adiciona caminho das imagens e vídeos ao JSON das embarcações
-def addImagesAndVideosToJson(json_list, sql_novo_seminovo, sql_share_modelo, sql_share_localidade):
-     
-    # Novo - Seminovo
-    for embarcacao in range(5):#sql_novo_seminovo:
-        embarcacoes_novo_seminovo.append({
-            'marca':'NX boats',     # PRECISA SER FEITO DIFERENTE
-            'modelos':[{
-                'nome': 'NX 250',
-                 'dados_tecnicos':{
-                    'peso':0,
-                    'cor':'azul',
-                    'tamanho':0,
-                },
-                'imagens':[],
-                'videos':[],
-                'tour':'Em análise.'
-            }],
-        })
-
-    for embarcacao in range(5):#sql_novo_seminovo:
-        embarcacoes_novo_seminovo.append({
-            'marca':'NX boats',
-            'modelos':[{
-                'nome': 'NX 250',
-                 'dados_tecnicos':{
-                    'peso':0,
-                    'cor':'azul',
-                    'tamanho':0,
-                },
-                'imagens':[],
-                'videos':[],
-                'tour':'Em análise.'
-            }],
-        })
-
-
-
-
-    # Share - por modelo
-    for embarcacao in range(5):#sql_share_modelo:
-        embarcacoes_share_modelo.append({
-            'modelo':'Focker 280 GT 2012',
-
-            'dados_tecnicos':{
-                'peso':0,
-                'cor':'azul',
-                'tamanho':0,
-            },
-            'imagens':[],
-            'videos':[],
-            'tour':'Em análise.'
-            
-        })
-
-    # Share - por localização
-    for embarcacao in range(5):#sql_share_localidade:
-        embarcacoes_share_localicazao.append({
-            'localidade':'bertioga',
-            'modelos':[{
-                'nome': 'NX 250',
-                 'dados_tecnicos':{
-                    'peso':0,
-                    'cor':'azul',
-                    'tamanho':0,
-                },
-                'imagens':[],
-                'videos':[],
-                'tour':'Em análise.'
-            }],
-            
-    })
-
-
-    for embarc in json_list:
-        for directory in dir_list_images:
-            if 'marca' in embarc:
-                if embarc['marca'].upper() == directory.upper():
-                    images = os.listdir(f'{images_path}/{directory.upper()}')
-                    for image in images:
-                        embarc['imagens'].append({'path':f'{images_path}/{directory.upper()}/{image}'})
-            elif 'modelo' in embarc:
-                if embarc['modelo'].upper() == directory.upper():
-                    images = os.listdir(f'{images_path}/{directory.upper()}')
-                    for image in images:
-                        embarc['imagens'].append({'path':f'{images_path}/{directory.upper()}/{image}'})
-            elif 'localidade' in embarc:
-                if embarc['localidade'].upper() == directory.upper():
-                    images = os.listdir(f'{images_path}/{directory.upper()}')
-                    for image in images:
-                        embarc['imagens'].append({'path':f'{images_path}/{directory.upper()}/{image}'})
-
-    for directory in dir_list_videos:
-        if 'marca' in embarc:
-            if embarc['marca'].upper() == directory.upper():
-                videos = os.listdir(f'{videos_path}/{directory.upper()}')
-                for video in videos:
-                    embarc['imagens'].append({'path':f'{videos_path}/{directory.upper()}/{video}'})
-        elif 'modelo' in embarc:
-            if embarc['modelo'].upper() == directory.upper():
-                videos = os.listdir(f'{videos_path}/{directory.upper()}')
-                for video in videos:
-                    embarc['imagens'].append({'path':f'{videos_path}/{directory.upper()}/{video}'})
-        elif 'localidade' in embarc:
-            if embarc['localidade'].upper() == directory.upper():
-                videos = os.listdir(f'{videos_path}/{directory.upper()}')
-                for video in videos:
-                    embarc['imagens'].append({'path':f'{videos_path}/{directory.upper()}/{video}'})
-
-
 ## Start
 
 
-# mydatabase = MC.connect(
-#     host='srv-dev',
-#     user='root',
-#     password='',
-#     database='allflags'
-# )
+mydatabase = MC.connect(
+    host='srv-dev',
+    user='root',
+    password='',
+    database='allflags'
+)
+
+sql_novos_seminovos = mydatabase.cursor(buffered=True)
+sql_share_localidade = mydatabase.cursor(buffered=True)
+sql_share_modelo = mydatabase.cursor(buffered=True)
+
+print('Conectado ao banco.')
 
 
-# sql_novos_seminovos = mydatabase.cursor()
-# sql_share = mydatabase.cursor()
 
-# print('Conectado ao banco.')
-
-
-# sql_novos_seminovos.execute("""
-#         SELECT E.id_embarcacao, E.modelo,
-#         M.nome_marina, M.estado, M.cidade, M.bairro,
-#         M.complemento
+sql_novos_seminovos.execute("""
+        SELECT E.id_embarcacao, E.modelo,
+        M.nome_marina, M.estado, M.cidade, M.bairro,
+        M.complemento
             
-#         FROM embarcacoes as E
-#         INNER JOIN marinas AS M ON M.id_marina = E.id_marina
+        FROM embarcacoes as E
+        INNER JOIN marinas AS M ON M.id_marina = E.id_marina
             
-#         WHERE M.ativo = 'S'
-# """)
-# sql_share_modelo.execute("""
-#         SELECT E.id_embarcacao, E.modelo,
-#         M.nome_marina, M.estado, M.cidade, M.bairro,
-#         M.complemento
+        WHERE M.ativo = 'S'
+""")
+sql_share_modelo.execute("""
+        SELECT E.id_embarcacao, E.modelo,
+        M.nome_marina, M.estado, M.cidade, M.bairro,
+        M.complemento
             
-#         FROM embarcacoes as E
-#         INNER JOIN marinas AS M ON M.id_marina = E.id_marina
+        FROM embarcacoes as E
+        INNER JOIN marinas AS M ON M.id_marina = E.id_marina
             
-#         WHERE M.ativo = 'S'
-# """)
-# sql_share_localidade.execute("""
-#         SELECT E.id_embarcacao, E.modelo,
-#         M.nome_marina, M.estado, M.cidade, M.bairro,
-#         M.complemento
+        WHERE M.ativo = 'S'
+""")
+sql_share_localidade.execute("""
+        SELECT E.id_embarcacao, E.modelo,
+        M.nome_marina, M.estado, M.cidade, M.bairro,
+        M.complemento
             
-#         FROM embarcacoes as E
-#         INNER JOIN marinas AS M ON M.id_marina = E.id_marina
+        FROM embarcacoes as E
+        INNER JOIN marinas AS M ON M.id_marina = E.id_marina
             
-#         WHERE M.ativo = 'S'
-# """)
+        WHERE M.ativo = 'S'
+""")
 
 
 
-# results_sql_novos_seminovos = sql_novos_seminovos.fetchall()
-# results_sql_share_modelo = sql_share.fetchall()
-# results_sql_share_localidade = sql_share_localidade.fetchall()
+results_sql_novos_seminovos = sql_novos_seminovos.fetchall()
+results_sql_share_modelo = sql_share_modelo.fetchall()
+results_sql_share_localidade = sql_share_localidade.fetchall()
 
 
 
 
 ######################################################## OPERAÇÕES ABAIXO
 
+# Novo - Seminovo
+for embarcacao in results_sql_novos_seminovos:
+    embarcacoes_novo_seminovo.append({
+        'marca': 'NX Boats',     # PRECISA SER FEITO DIFERENTE
+        'modelos':[{
+            'nome': embarcacao[1],
+            'dados_tecnicos':{
+                'peso':embarcacao[1],
+                'cor': embarcacao[1],
+                'tamanho': embarcacao[1],
+            },
+            'imagens':[],
+            'videos':[],
+            'tour':'Em análise.'
+        }],
+    })
 
+# Share - por modelo
+for embarcacao in results_sql_share_modelo:
+    embarcacoes_share_modelo.append({
+        'modelo':embarcacao[1],
 
-addImagesAndVideosToJson(embarcacoes_novo_seminovo, '', '', '')
-addImagesAndVideosToJson(embarcacoes_share_modelo, '', '', '')
-addImagesAndVideosToJson(embarcacoes_share_localicazao, '', '', '')
+        'dados_tecnicos':{
+            'peso':embarcacao[1],
+            'cor':embarcacao[1],
+            'tamanho':embarcacao[1],
+        },
+        'imagens':[],
+        'videos':[],
+        'tour':'Em análise.'
+        
+    })
+
+# Share - por localização
+for embarcacao in results_sql_share_localidade:
+    embarcacoes_share_localicazao.append({
+        'localidade':embarcacao[1],
+        'modelos':[{
+            'nome': embarcacao[1],
+                'dados_tecnicos':{
+                'peso':embarcacao[1],
+                'cor':embarcacao[1],
+                'tamanho':embarcacao[1],
+            },
+            'imagens':[],
+            'videos':[],
+            'tour':'Em análise.'
+        }],
+        
+})
+    
+
+for embarc in embarcacoes_novo_seminovo:
+    for directory in dir_list_images:
+        if embarc['marca'].upper() == directory.upper():
+            for modelos in embarc['modelos']:
+                try:
+                    images = os.listdir(f'{images_path}/{directory.upper()}/{modelos["nome"].rstrip()}')
+                    for image in images:
+                        modelos['imagens'].append({'path':f'{images_path}/{directory.upper()}/{modelos["nome"].rstrip()}/{image}'})
+                except FileNotFoundError:
+                    print('Uma embarcação não foi encontrada na pasta de imagens.')
+                except OSError:
+                    print('Uma embarcação gerou um erro no seu nome de diretório das imagens.')
+
+    for directory in dir_list_videos:
+        if embarc['marca'].upper() == directory.upper():
+            for modelos in embarc['modelos']:
+                try:
+                    videos = os.listdir(f'{videos_path}/{directory.upper()}/{modelos["nome"].rstrip()}')
+                    for video in videos:
+                        modelos['videos'].append({'path':f'{videos_path}/{directory.upper()}/{modelos["nome"].rstrip()}/{video}'})
+                except FileNotFoundError:
+                    print('Uma embarcação não foi encontrada na pasta de vídeos.')
+                except OSError:
+                    print('Uma embarcação gerou um erro no seu nome de diretório dos vídeos.')
 
 
 
